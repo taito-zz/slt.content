dbb7bac146d6a043b89370c28e236421250920ad
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-11-12 07:10:39 +0200

    release 0.15

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='slt.content',
-    version='0.14',
+    version='0.15',
     description="Provides content types for SLT shopping site.",
     long_description=open("README.rst").read(),
     classifiers=[
